5aee74da865356bef89635a960bdf5763dcf1a42
Alyson Ryan Ullirsch <sdhuahdsaih@gmail.com>
2024-12-21 17:45:36 -0300

    create

---
+++ Data/AppDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SiteBlazorServer.Models;
+
+namespace SiteBlazorServer.Data;
+
+public class AppDataContext : DbContext
+{
+    public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,28 +1,39 @@
+using Microsoft.EntityFrameworkCore;
 using SiteBlazorServer.Components;
+using SiteBlazorServer.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+BuilderConfigure(builder);
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+AppConfigure(app);
 
 app.Run();
+
+void AppConfigure(WebApplication app)
+{
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+        app.UseHsts();
+    }
+    
+    app.UseHttpsRedirection();
+
+
+    app.UseAntiforgery();
+
+    app.MapStaticAssets();
+    app.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode();
+}
+
+void BuilderConfigure(WebApplicationBuilder builder)
+{
+    builder.Services.AddRazorComponents()
+        .AddInteractiveServerComponents();
+
+    builder.Services.AddDbContext<AppDataContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
+}
